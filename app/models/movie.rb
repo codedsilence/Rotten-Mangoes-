e69023635092a3bc668fdd03829c3db6bacf85c6
codedsilence <patrick.sawtell@gmail.com>
2015-10-28 21:38:52 +0000

    helper methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -23,6 +23,10 @@ class Movie < ActiveRecord::Base
 	#custom validation 
 	validate :release_date_is_in_the_past
 
+	def review_average 
+		reviews.sum(:rating_out_of_ten)/reviews.size
+	end
+
 	protected
 
 	def release_date_is_in_the_past
